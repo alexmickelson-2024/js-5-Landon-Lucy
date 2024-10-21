037641245fedf39cbb7b84464621bdc7b00b6473
Landon Ginn <landontg713@gmail.com>
2024-10-20 23:45:20 -0600

    Almost Perfect

--- site.js
+++ site.js
@@ -15,49 +15,56 @@ import { products } from "./products.js";
       </div> */
 }
 
+function CreateCard(product) {
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+  cardElement.id = `card-${product.title}`;
+  cardElement.draggable = true;
+
+  const cardImageElement = document.createElement("div");
+  cardImageElement.classList.add("card-img");
+  cardImageElement.style = `background-image: url('${product.image}')`;
+
+  const cardContentElement = document.createElement("div");
+  cardContentElement.classList.add("card-content");
+
+  const cardTitleElement = document.createElement("div");
+  cardTitleElement.classList.add("card-title");
+  cardTitleElement.textContent = `${product.title}`;
+
+  const cardDescriptionElement = document.createElement("div");
+  cardDescriptionElement.classList.add("card-description");
+  cardDescriptionElement.textContent = `${product.description}`;
+
+  const cardPriceElement = document.createElement("div");
+  cardPriceElement.classList.add("card-price");
+  cardPriceElement.textContent = `${product.price}`;
+
+  const cardQuantityElement = document.createElement("div");
+  cardQuantityElement.classList.add("card-quantity");
+  cardQuantityElement.textContent = `${product.quantity}`;
+  cardQuantityElement.id = `${product.title}-quantity`;
+
+  cardContentElement.appendChild(cardTitleElement);
+  cardContentElement.appendChild(cardDescriptionElement);
+  cardContentElement.appendChild(cardPriceElement);
+  cardContentElement.appendChild(cardQuantityElement);
+
+  cardElement.appendChild(cardImageElement);
+  cardElement.appendChild(cardContentElement);
+
+  cardElement.addEventListener("dragstart", dragStart);
+
+  return cardElement;
+}
+
 function LoadItemsList() {
   products.forEach((product) => {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.id = `card-${product.title}`;
-    cardElement.draggable = true;
-
-    const cardImageElement = document.createElement("div");
-    cardImageElement.classList.add("card-img");
-    cardImageElement.style = `background-image: url('${product.image}')`;
-
-    const cardContentElement = document.createElement("div");
-    cardContentElement.classList.add("card-content");
-
-    const cardTitleElement = document.createElement("div");
-    cardTitleElement.classList.add("card-title");
-    cardTitleElement.textContent = `${product.title}`;
-
-    const cardDescriptionElement = document.createElement("div");
-    cardDescriptionElement.classList.add("card-description");
-    cardDescriptionElement.textContent = `${product.description}`;
-
-    const cardPriceElement = document.createElement("div");
-    cardPriceElement.classList.add("card-price");
-    cardPriceElement.textContent = `${product.price}`;
-
-    const cardQuantityElement = document.createElement("div");
-    cardQuantityElement.classList.add("card-quantity");
-    cardQuantityElement.textContent = `${product.quantity}`;
-
-    cardContentElement.appendChild(cardTitleElement);
-    cardContentElement.appendChild(cardDescriptionElement);
-    cardContentElement.appendChild(cardPriceElement);
-    cardContentElement.appendChild(cardQuantityElement);
-
-    cardElement.appendChild(cardImageElement);
-    cardElement.appendChild(cardContentElement);
-
-    cardElement.addEventListener("dragstart", dragStart);
+    const cardElementCreated = CreateCard(product);
 
     const productsContainerElement =
       document.getElementById("products-container");
-    productsContainerElement.appendChild(cardElement);
+    productsContainerElement.appendChild(cardElementCreated);
   });
 }
 
@@ -80,7 +87,58 @@ function dropCart(e) {
 
   const containerTargetElelement = document.getElementById("cartList");
 
-  containerTargetElelement.appendChild(draggable);
+  const draggableCopy = products.filter((product) => {
+    if (draggable.id === `card-${product.title}`) return product;
+  });
+
+  const draggableCopyElement = CreateCard(draggableCopy[0]);
+  draggableCopyElement.id = `${draggableCopyElement.id}-copy`;
+
+  const totalElement = document.getElementById("amount");
+  totalElement.textContent = parseFloat(parseFloat(totalElement.textContent) + parseFloat(draggable.lastChild.childNodes[2].textContent)).toFixed(2);
+
+
+  if (
+    !containerTargetElelement.contains(
+      document.getElementById(draggableCopyElement.id)
+    )
+  ) {
+    containerTargetElelement.appendChild(draggableCopyElement);
+
+    draggableCopyElement.lastChild.removeChild(
+      draggableCopyElement.lastChild.lastChild
+    );
+
+    const cardQuantityElementCopy = document.createElement("div");
+    cardQuantityElementCopy.classList.add("card-quantity");
+    cardQuantityElementCopy.textContent = `1`;
+    cardQuantityElementCopy.id = `${draggableCopyElement.id}-quantity`;
+
+    draggableCopyElement.lastChild.appendChild(cardQuantityElementCopy);
+
+    if (draggable.lastChild.lastChild.textContent - 1 === 0) {
+      draggable.remove();
+    }
+
+    draggable.lastChild.lastChild.textContent = `${
+      draggable.lastChild.lastChild.textContent - 1
+    }`;
+  } else {
+    const existingCopyElement = document.getElementById(
+      draggableCopyElement.id
+    );
+
+    if (draggable.lastChild.lastChild.textContent - 1 === 0) {
+      draggable.remove();
+    }
+
+    existingCopyElement.lastChild.lastChild.textContent = `${
+      existingCopyElement.lastChild.lastChild.textContent - -1
+    }`;
+    draggable.lastChild.lastChild.textContent = `${
+      draggable.lastChild.lastChild.textContent - 1
+    }`;
+  }
 }
 
 function dropProduct(e) {
@@ -88,9 +146,61 @@ function dropProduct(e) {
 
   const draggable = document.getElementById(id);
 
-  const containerTargetElelement = document.getElementById("products-container");
+  const containerTargetElelement = document.getElementById("productList");
 
-  containerTargetElelement.appendChild(draggable);
+  const draggableCopy = products.filter((product) => {
+    if (draggable.id === `card-${product.title}-copy`) return product;
+  });
+
+  const draggableCopyElement = CreateCard(draggableCopy[0]);
+  console.log(draggableCopyElement.id);
+
+  const totalElement = document.getElementById("amount");
+  totalElement.textContent = parseFloat(parseFloat(totalElement.textContent) - parseFloat(draggable.lastChild.childNodes[2].textContent)).toFixed(2);
+
+  if (
+    !containerTargetElelement.contains(
+      document.getElementById(draggableCopyElement.id)
+    )
+  ) {
+    containerTargetElelement.appendChild(draggableCopyElement);
+
+    draggableCopyElement.lastChild.removeChild(
+      draggableCopyElement.lastChild.lastChild
+    );
+
+    const cardQuantityElementCopy = document.createElement("div");
+    cardQuantityElementCopy.classList.add("card-quantity");
+    cardQuantityElementCopy.textContent = `1`;
+    cardQuantityElementCopy.id = `${draggableCopyElement.id}-quantity`;
+
+    draggableCopyElement.lastChild.appendChild(cardQuantityElementCopy);
+
+    if (draggable.lastChild.lastChild.textContent - 1 === 0) {
+      draggable.remove();
+    }
+
+    draggable.lastChild.lastChild.textContent = `${
+      draggable.lastChild.lastChild.textContent - 1
+    }`;
+  } else {
+    const existingCopyElement = document.getElementById(
+      draggableCopyElement.id
+    );
+    console.log(existingCopyElement);
+
+    if (draggable.lastChild.lastChild.textContent - 1 === 0) {
+      draggable.remove();
+    }
+
+    existingCopyElement.lastChild.lastChild.textContent = `${
+      existingCopyElement.lastChild.lastChild.textContent - -1
+    }`;
+    draggable.lastChild.lastChild.textContent = `${
+      draggable.lastChild.lastChild.textContent - 1
+    }`;
+  }
+
 }
 
 LoadItemsList();
@@ -106,3 +216,5 @@ const productContainerElement = document.getElementById("products-container");
 productContainerElement.addEventListener("dragenter", dragEnter);
 productContainerElement.addEventListener("dragover", dragOver);
 productContainerElement.addEventListener("drop", dropProduct);
+
+
